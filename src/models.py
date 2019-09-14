cb3b95506e35cdf17c4f11f793b40660b5242d48
Homero Luzardo <homeroeluzardo@gmail.com>
2019-09-14 16:39:09 +0000

    Update Database

--- src/models.py
+++ src/models.py
@@ -1,34 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-# from eralchemy import render_er
+import os, sys, enum
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Messurments(enum.Enum):
+    mililiter = "ml"
+    miligram = "mg"
+    ounce = "oz"
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+class Category(enum.Enum):
+    meet = "meet"
+    grane = "grane"
+    vedgetable = "vedgetable"
+    dary = "dary"
+    fruit = "fruit"
+    nut = "nut"
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+class Market(enum.Enum):
+    publix = "publix"
+    walmart = "walmart"
+    president = "president"
+    whole_foods = "whole_foods"
 
 
-class PivRecIng(db.Model):
-    __tablename__ = 'pivRecIng'
-    id=Column(Integer, primary_key=True)
-    idFallower = Column(Integer, ForeignKey('person.fallowersId'))
-    idFallowing = Column(Integer, ForeignKey('person.fallowingId'))
+
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -47,17 +42,36 @@ class Profile(db.Model):
     peso = db.Column(db.Integer, nullable=False)
     talla = db.Column(db.Integer, nullable=False)
     alergia = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship("User", back_populates="profile")
+    recipes = db.relationship("Receta_Ingrediente", back_populates="profile")
+    stocks = db.relationship("Stock", back_populates="profile")
 
     def __repr__(self):
          return '<Profile %r>' % self.id
+
+class Stock(db.Model):
+    __tablename__ = 'stock'
+    id=db.Column(db.Integer, primary_key=True)
+    id_profile = db.Column(db.Integer, db.ForeignKey('profile.id'))
+    id_ingrediente = db.Column(db.Integer, db.ForeignKey('ingrediente.id'))
+    quantity = db.Column(db.Float, nullable=False)
+
+
+class Receta_Ingrediente(db.Model):
+    __tablename__ = 'receta_ingrediente'
+    id=db.Column(db.Integer, primary_key=True)
+    id_receta = db.Column(db.Integer, db.ForeignKey('receta.id'))
+    id_ingrediente = db.Column(db.Integer, db.ForeignKey('ingrediente.id'))
+    autor = db.Column(db.Integer, db.ForeignKey('profile.id'))
+    quantity = db.Column(db.Float, nullable=False)
+    messurment = db.Column(db.Enum(Messurments), nullable=False)
 
 class Receta(db.Model):
     __tablename__ = 'receta'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    caloria = db.Column(db.Integer, nullable=False)
+    calory = db.Column(db.Integer, nullable=False)
     image = db.Column(db.String(80), unique=False, nullable=False)
 
     def __repr__(self):
@@ -68,9 +82,19 @@ class Ingrediente(db.Model):
     __tablename__ = 'ingrediente'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    tipo = db.Column(db.String(80), unique=False, nullable=False)
-    caloria = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.Enum(Category), nullable=False)
+    calory = db.Column(db.Integer, nullable=False)
+    prices = db.relationship("Price", back_populates="ingrediente")
 
     def __repr__(self):
-         return '<Receta %r>' % self.name
+         return '<Ingrediente %r>' % self.name
 
+class Price(db.Model):
+    __tablename__ = 'price'
+    id = db.Column(db.Integer, primary_key=True)
+    market_name = db.Column(db.Enum(Market), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    id_ingrediente = db.Column(db.Integer, db.ForeignKey('ingrediente.id'))
+
+    def __repr__(self):
+         return '<Price %r>' % self.name
