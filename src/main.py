41e3352bc6e60811dd48fa27fe28cf009ce7d024
mmayor <mmayor200610@gmail.com>
2019-09-24 22:41:33 +0000

    update0923

--- src/main.py
+++ src/main.py
@@ -2,13 +2,18 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, Request, json
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+import requests
+from models import Receta
+from models import Ingrediente
+import copy
+# from models import Receta_Ingrediente
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,9 +24,11 @@ db.init_app(app)
 CORS(app)
 
 # Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+# @app.errorhandler(Exception)
+# def handle_invalid_usage(error):
+#     print(error)
+
+#     return error.status_code
 
 # generate sitemap with all your endpoints
 @app.route('/')
@@ -37,7 +44,107 @@ def handle_person():
 
     return jsonify(response_body), 200
 
+@app.route('/receta', methods=['GET'])
+def handle_receta():
+
+    headers = {"Content-type": "application/json"}
+    # data = curl -i "https://api.edamam.com/search\?q\=fish\&app_id\=$\{YOUR_APP_ID\}\&app_key\=$\{YOUR_APP_KEY\}"
+    # data = requests.get('https://api.edamam.com/search\?q\=fish\&app_id\=$\{YOUR_APP_ID\}\&app_key\=$\{YOUR_APP_KEY\}')
+    url = 'https://api.edamam.com/search?q=pork&app_id=%24%7BYOUR_APP_ID%7D&app_key=%24%7BYOUR_APP_KEY%7D'
+    response = requests.get(url=url)
+    rJonson = response.json()
+
+    response_body = {
+        "hello": "receta"
+    }
+
+    arryRecetaName = []
+    arrIngredientName = []
+    dicReceta = {}
+
+    dicReceta['name'] = ""
+    dicReceta['ingredient'] = []
+    dicReceta['calories'] = 0
+
+
+    for x in rJonson['hits']:
+        # arryRecetaName.append(x['recipe']['label'])
+        dicReceta['name'] = x['recipe']['label']
+        dicReceta['calories'] = x['recipe']['calories']
+        for y in (x['recipe']['ingredients']):
+           # arrIngredientName.append(x['recipe']['ingredients'])
+            # print(y['food'])
+            dicReceta['ingredient'].append(y['food'])
+
+       # print(dicReceta)
+
+        dicRecetaTemp = copy.deepcopy(dicReceta)
+        arryRecetaName.append(dicRecetaTemp)
+
+        dicReceta['ingredient'] = []
+
+
+    # arryRecetaName.append(dicReceta)
+    # print(arryRecetaName)
+    # print ( ((rJonson['hits'])[0])['recipe'].keys() )
+    # return jsonify(response), 200
+    # return rJonson['hits']
+    # print(len(arryRecetaName))
+    # print(arryRecetaName)
+    # print(arrIngredientName)
+    ## INSERT  RECETA
+    #receta1 = Contacts(full_name=body["full_name"], email=body["email"], agenda_slug=body["agenda_slug"], address=body["address"], phone=body["phone"])
+    #db.session.add(user1)
+    #db.session.commit()
+
+    ## INSERT NEW Receta
+
+    '''
+    for receta in arryRecetaName:
+
+            recetaTemp = Receta.query.filter_by(name=receta['name']).first()
+            if recetaTemp is None:
+                receta1 = Receta(name=receta['name'], image='image',  calory=receta['calories'])
+                db.session.add(receta1)
+                db.session.commit()
+                recetaTemp = None
+
+    '''
+    ## INSERT NEW INGREDIENTE
+
+    for receta in arryRecetaName:
+
+        recetaTemp = Receta.query.filter_by(name=receta['name']).first()
+        receta1 = Receta(name=receta['name'], image='image',  calory=receta['calories'])
+        if recetaTemp is None:
+
+                db.session.add(receta1)
+                db.session.commit()
+                recetaTemp = None
+
+
+        for ingrediente in (receta['ingredient']):
+
+            ingredienteTemp = Ingrediente.query.filter_by(name=ingrediente).first()
+            ingrediente1 = Ingrediente(name=ingrediente,calory=10, category='meet', prices=[])
+            if ingredienteTemp is  None:
+
+                print(ingrediente1)
+                db.session.add(ingrediente1)
+                db.session.commit()
+                ingredienteTemp = None
+
+            ingrediente1.ingredientes.append(receta1)
+            # receta1.ingredientes[ingrediente1]
+            db.session.commit()
+
+    ## INSERT Receta-Ingrediente
+
+    return rJonson, 200
+
+    # return arryTemp
+
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT)
+    app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,34 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-# from eralchemy import render_er
+import os, sys, enum
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
-
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+tags = db.Table('tags',
+    db.Column('receta_id', db.Integer, db.ForeignKey('receta.id'), primary_key=True),
+    db.Column('ingrediente_id', db.Integer, db.ForeignKey('ingrediente.id'), primary_key=True)
+)
 
 
-class PivRecIng(db.Model):
-    __tablename__ = 'pivRecIng'
-    id=Column(Integer, primary_key=True)
-    idFallower = Column(Integer, ForeignKey('person.fallowersId'))
-    idFallowing = Column(Integer, ForeignKey('person.fallowingId'))
+class Messurments(enum.Enum):
+    mililiter = "ml"
+    miligram = "mg"
+    ounce = "oz"
+
+class Category(enum.Enum):
+    meet = "meet"
+    grane = "grane"
+    vedgetable = "vedgetable"
+    dary = "dary"
+    fruit = "fruit"
+    nut = "nut"
+
+class Market(enum.Enum):
+    publix = "publix"
+    walmart = "walmart"
+    president = "president"
+    whole_foods = "whole_foods"
+
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -47,30 +46,58 @@ class Profile(db.Model):
     peso = db.Column(db.Integer, nullable=False)
     talla = db.Column(db.Integer, nullable=False)
     alergia = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship("User", back_populates="profile")
+    # recetas = db.relationship("Receta", back_populates="autor")
 
     def __repr__(self):
          return '<Profile %r>' % self.id
 
+class Stock(db.Model):
+    __tablename__ = 'stock'
+    id=db.Column(db.Integer, primary_key=True)
+    id_profile = db.Column(db.Integer, db.ForeignKey('profile.id'))
+    # id_ingrediente = db.Column(db.Integer, db.ForeignKey('ingrediente.id'))
+    quantity = db.Column(db.Float, nullable=False)
+'''
+class Receta_Ingrediente(db.Model):
+    __tablename__ = 'receta_ingrediente'
+    id=db.Column(db.Integer, primary_key=True)
+    id_receta = db.Column(db.Integer, db.ForeignKey('receta.id'))
+    id_ingrediente = db.Column(db.Integer, db.ForeignKey('ingrediente.id'))
+    quantity = db.Column(db.Float, nullable=False)
+    messurment = db.Column(db.Enum(Messurments), nullable=False)
+'''
+
 class Receta(db.Model):
     __tablename__ = 'receta'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    caloria = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    calory = db.Column(db.Integer, nullable=False)
     image = db.Column(db.String(80), unique=False, nullable=False)
-
+    # autor = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=True)
+    relacion = db.relationship('Ingrediente', secondary=tags, backref=db.backref('ingredientes', lazy='dynamic'))
     def __repr__(self):
          return '<Receta %r>' % self.name
-
 
 class Ingrediente(db.Model):
     __tablename__ = 'ingrediente'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    tipo = db.Column(db.String(80), unique=False, nullable=False)
-    caloria = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    category = db.Column(db.Enum(Category), nullable=False)
+    calory = db.Column(db.Integer, nullable=False)
+    prices = db.relationship("Price", backref="ingrediente")
+    # stock = db.relationship("Stock", back_populates="id_ingrediente")
 
     def __repr__(self):
-         return '<Receta %r>' % self.name
+         return '<Ingrediente %r>' % self.name
 
+class Price(db.Model):
+    __tablename__ = 'price'
+    id = db.Column(db.Integer, primary_key=True)
+    market_name = db.Column(db.Enum(Market), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    id_ingrediente = db.Column(db.Integer, db.ForeignKey('ingrediente.id'))
+
+    def __repr__(self):
+         return '<Price %r>' % self.market_name
